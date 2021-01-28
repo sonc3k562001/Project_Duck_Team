07cc84987fc6ba00e2d21428dcb99b95e2911f51
an <54616933+tranthian-snake@users.noreply.github.com>
2021-01-28 22:15:05 +0700

    login

---
+++ Jewellry/Jewellry/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Jewellry.Models;
+
+namespace Jewellry.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+

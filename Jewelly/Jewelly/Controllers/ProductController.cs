6e39c6bf69fc8a555affae62bae2c2fcfccab18e
PhamQuynh048 <62494419+PhamQuynh048@users.noreply.github.com>
2021-02-20 20:10:44 +0700

    update

---
+++ Jewelly/Jewelly/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jewelly.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("product/bracelet")]
+        public IActionResult Bracelets()
+        {
+            return View();
+        }
+
+        public IActionResult Lockets()
+        {
+            return View();
+        }
+
+        public IActionResult Necklaces()
+        {
+            return View();
+        }
+         public IActionResult Rings()
+        {
+            return View();
+        }
+
+        public IActionResult Product_detail()
+        {
+            return View();
+        }
+        public IActionResult Product_list()
+        {
+            return View();
+        }
+
+    }
+}
